62b5eb54c42f41ec04eeccd72f4893fec5a7bf8d
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-24 14:43:55 -0500

    Se agrega carpetas
    Se agrega todas las carpetas

--- src/models/customer.model.ts
+++ src/models/customer.model.ts
@@ -1,14 +1,13 @@
-import { DocumentTypeModel } from './';
-
+import {DocumentTypeModel} from "./"
 export interface CustomerModel {
-  id: string;
-  documentType: DocumentTypeModel;
-  document: string;
-  fullName: string;
-  email: string;
-  phone: string;
-  password: string;
-  avatarUrl?: string;
-  state: boolean;
-  deletedAt?: Date | number;
+    id: string;
+    documentType: DocumentTypeModel;
+    document: string;
+    fullName: string;
+    email: string;
+    phone: string;
+    password: string;
+    avatarUrl?: string;
+    state: boolean;
+    deletedAt?: Date | number;
 }
